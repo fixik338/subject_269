d83db9c3f110430161cb1fdf1b657d5739f0fd15
fixik338 <0firter0@gmail.com>
2019-10-09 00:12:25 +0400

    myabe woring thing

--- lb 2 kurs/NEW_lab 1.9.6.py
+++ lb 2 kurs/NEW_lab 1.9.6.py
@@ -1,12 +1,27 @@
-from numpy import *
-n = 5
+import numpy as np
+from random import *
+n = 6
 p = 1
-r = 0.1
-b = 0.01
-A = zeros((n, n))
+r = 1
+b = 0.1
+eps = 10**-3
+Y0 = 0
+Y1 = 0.1
+x0 = np.zeros(n)
+A = np.zeros((n, n))
 for i in range(1, n):
+    x0[i] = random()
     A[i][i] = 6.5 * i ** (p / 3)
     for j in range(1, n):
-        A[i][j] = b*exp(-r/((i + j)**r))
+        A[i][j] = b*np.exp(-r/((i + j)**r))
 A = A[1:n, 1:n]
-print(A)
+x0 = x0[1:n]
+k = 0
+while np.linalg.norm(Y1 - Y0) < eps:
+    Y0 = Y1
+    x0norm = np.dot(x0, x0)**(1/2)
+    e1 = x0/x0norm
+    x0 = A*e1
+    Y1 = np.dot(x0, e1)
+    k += 1
+print(x0, Y1, k)
