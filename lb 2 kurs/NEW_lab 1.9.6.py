9840485922ce32aade96a46083d4eba4ba7e5761
fixik338 <0firter0@gmail.com>
2019-10-09 14:04:34 +0400

    greato succex maybe

--- lb 2 kurs/NEW_lab 1.9.6.py
+++ lb 2 kurs/NEW_lab 1.9.6.py
@@ -1,27 +1,28 @@
 import numpy as np
 from random import *
+
 n = 6
 p = 1
-r = 1
-b = 0.1
-eps = 10**-3
-Y0 = 0
-Y1 = 0.1
+r = 0.1
+b = 0.01
+eps = 10 ** -3
+Y0 = 0.1
+Y1 = 0.01
 x0 = np.zeros(n)
 A = np.zeros((n, n))
 for i in range(1, n):
-    x0[i] = random()
+    x0[i] = np.random.uniform(0, 1)
     A[i][i] = 6.5 * i ** (p / 3)
     for j in range(1, n):
-        A[i][j] = b*np.exp(-r/((i + j)**r))
+        A[i][j] = b * np.exp(-r / ((i + j) ** r))
 A = A[1:n, 1:n]
 x0 = x0[1:n]
 k = 0
-while np.linalg.norm(Y1 - Y0) < eps:
-    Y0 = Y1
-    x0norm = np.dot(x0, x0)**(1/2)
-    e1 = x0/x0norm
-    x0 = A*e1
+while np.linalg.norm(Y1 - Y0) > eps and k < 10:
+    x0norm = np.dot(x0, x0) ** (1 / 2)
+    e1 = x0 / x0norm
+    x0 = np.dot(A, e1)
     Y1 = np.dot(x0, e1)
     k += 1
-print(x0, Y1, k)
+print(np.linalg.eig(A))
+print(Y1, k)
--- lb 2 kurs/Черновик.py
+++ lb 2 kurs/Черновик.py
@@ -1,11 +1,31 @@
 import numpy as np
+from random import *
 
-a = np.array([[2, 1, -1, 2],
-             [2, 3, -3, 4],
-             [8, 3, 2, 2],
-             [8, 1, 1, 5]])
-A = np.array([[2, 1, -1, 2],
-              [2, 3, -3, 4],
-              [8, 3, 2, 2],
-              [8, np.NaN, 1, 5]])
-print(a > 1)
+n = 6
+p = 1
+r = 1
+b = 0.1
+eps = 10 ** -3
+Y0 = 0.1
+Y1 = 0.0001
+x0 = np.zeros(n)
+A = np.zeros((n, n))
+for i in range(1, n):
+    x0[i] = np.random.uniform(0, 1)
+    A[i][i] = i
+    for j in range(1, n):
+        if i != j:
+            A[i][j] = 0
+A = A[1:n, 1:n]
+x0 = x0[1:n]
+print(A)
+k = 0
+while np.linalg.norm(Y1 - Y0) < eps:
+    Y0 = Y1
+    x0norm = np.dot(x0, x0) ** (1 / 2)
+    e1 = x0 / x0norm
+    x1 = np.dot(A, e1)
+    Y1 = np.dot(x1, e1)
+    x0 = np.copy(x1)
+    k += 1
+print(x0, Y1, k)
