3a3da6eabbd1c338a71b3c0cca297a38665ddd9b
fixik338 <0firter0@gmail.com>
2019-11-03 12:05:36 +0400

    ubdate

--- NEW_lab 1.8 LU.py
+++ NEW_lab 1.8 LU.py
@@ -4,11 +4,14 @@ r = 1
 t = 1
 N = 10
 
-A = np.zeros((N+1, N+1))
-for i in range(1, N+1):
-    for j in range(1, N+1):
-        A[i][j] = 10 ** -3 * np.exp(-r / (abs(i + j) ** t))
+A = np.zeros((N, N))
+B = np.zeros(N)
+for i in range(0, N):
+    for j in range(0, N):
+        A[i][j] = 10 ** -3 * np.exp(-r / (abs((i+1) + (j+1)) ** t))
         A[i][i] = 8.5 * i ** (p / 3)
+    B[i] = (7.5 * i + 1) ** (t / 2)
+print(np.linalg.solve(A, B))
 U = A
 L = np.tril(A)
 for i in range(10):
