f5fd97c244af33562261fcbfef2a4a208a9732a4
fixik338 <0firter0@gmail.com>
2019-11-12 23:31:34 +0400

    ubdate 12,11,2019

--- lb 2 kurs/NEW_lab 1.2.6.py
+++ lb 2 kurs/NEW_lab 1.2.6.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sympy as sp
 import matplotlib.pyplot as plt
 
 eps = 10 ** -6
@@ -7,10 +8,9 @@ k = 1
 j = 1
 a = 0
 b = 1
-x0 = 0.2
 
 
-def dih(eps, m, k, j, a, b, x0):
+def dih(eps, m, k, j, a, b):
     n = 0
     fa = (1 - a) ** (1 / j) - np.tan((np.pi * a ** m) / 4) ** k
     while np.fabs(a - b) > eps:
@@ -24,27 +24,52 @@ def dih(eps, m, k, j, a, b, x0):
             b = c
     x = (a + b) / 2
     fx = (1 - x) ** (1 / j) - np.tan((np.pi * x ** m) / 4) ** k
-    print("Знач. функции: ", fx, "\nОТВЕТ:", x, "\nИтераций:", n)
+    return fx, x, n
 
 
-def sek(eps, m, k, j, x0):
+def sek(eps, m, k, j):
     i = 0
     imax = 10
-    x1 = x0 - 90*eps
+    x0 = 0.2
+    x1 = x0 - 90 * eps
     fx0 = (1 - x0) ** (1 / j) - np.tan((np.pi * x0 ** m) / 4) ** k
     while i < imax:
         fx1 = (1 - x1) ** (1 / j) - np.tan((np.pi * x1 ** m) / 4) ** k
-        x = x1 - ((x1 - x0)/(fx1 - fx0))*fx1
+        x = x1 - ((x1 - x0) / (fx1 - fx0)) * fx1
         eps1 = np.fabs(x - x1)
         if eps1 > eps:
             x0 = x1
             fx0 = fx1
             x1 = x
         i += 1
-    fx = (1 - x) ** (1 / j) - np.tan((np.pi * x ** m) / 4) ** k
+    fx = (1 - x1) ** (1 / j) - np.tan((np.pi * x1 ** m) / 4) ** k
     # plt.show()
-    print("Знач. функции: ", fx, "\nОТВЕТ:", x, "\nИтераций:", i)
+    return fx, x1, i
 
 
-dih(eps, m, k, j, a, b, x0)
-sek(eps, m, k, j, x0)
+def kas(eps, m, k, j):
+    i = 0
+    imax = 10
+    x0 = 0.2
+    fx0 = (1 - x0) ** (1 / j) - np.tan((np.pi * x0 ** m) / 4) ** k
+    dfx0 = -np.pi * (np.tan(np.pi * x0 / 4) ** 2 + 1) / 4 - 1
+
+    def f(x0):
+        f = (1 - x0) ** (1 / j) - np.tan((np.pi * x0 ** m) / 4) ** k
+        return f
+
+    def df(x0):
+        df = -np.pi * (np.tan(np.pi * x0 / 4) ** 2 + 1) / 4 - 1
+        return df
+
+    x1 = x0 - (f(x0) / df(x0))
+    while np.fabs(x1 - x0) > eps or i < imax:
+        x1 = x0 - (f(x0) / df(x0))
+        x0 = x1
+        i += 1
+    fx = (1 - x1) ** (1 / j) - np.tan((np.pi * x1 ** m) / 4) ** k
+    return fx, x1, i
+
+
+print("Дихотомия:", dih(eps, m, k, j, a, b), "\nМетод секущих:", sek(eps, m, k, j), "\nМетод касательных:",
+      kas(eps, m, k, j))
