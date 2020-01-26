7f783abdb009752a665b5afa02c633fa9c20751e
fixik338 <0firter0@gmail.com>
2020-01-26 12:04:45 +0400

    Изменена функция L

--- lb 2 kurs/Задание с билета, Игорь.py
+++ lb 2 kurs/Задание с билета, Игорь.py
@@ -2,9 +2,10 @@ from math import *
 import numpy as np
 def L(Si, k):
     n = 0
-    l0 = Si[0]
+    l0 = []
+    l0.append(Si[0])
     while n < k:
-        l0 += Si[n+1] - Si[n]
+        l0.append(Si[n+1] - Si[n])
         n += 1
         return l0
 
@@ -12,6 +13,7 @@ def L(Si, k):
 
 
 k = 10
+
 a = 0
 b = pi/2
 x = []
